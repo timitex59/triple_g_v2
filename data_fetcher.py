c537679f0ddb0b538578d85e96af3529cc7725c0
pruchi59 <57199954+pruchi59@users.noreply.github.com>
2025-12-23 20:56:04 +0100

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -42,7 +42,13 @@ CACHE_FILE = "market_cache.pkl"
 
 # Liste complète des paires (Union des besoins des 3 scripts)
 PAIRS = [
-     "USDJPY=X", "GBPJPY=X", "EURJPY=X", "AUDJPY=X", "NZDJPY=X", "CADJPY=X", "CHFJPY=X"
+    "EURUSD=X", "GBPUSD=X", "USDJPY=X", "AUDUSD=X", "NZDUSD=X", "USDCAD=X", "USDCHF=X",
+    "EURGBP=X", "EURJPY=X", "GBPJPY=X", "AUDJPY=X", "NZDJPY=X", "CADJPY=X", "CHFJPY=X",
+    "EURAUD=X", "EURCAD=X", "EURNZD=X", "EURCHF=X",
+    "GBPAUD=X", "GBPCAD=X", "GBPNZD=X", "GBPCHF=X",
+    "AUDNZD=X", "AUDCAD=X", "AUDCHF=X",
+    "NZDCAD=X", "NZDCHF=X",
+    "CADCHF=X",
 ]
 
 # --- TRADINGVIEW ENGINE ---
